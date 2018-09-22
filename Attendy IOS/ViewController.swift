720ac137252fb02a67b5157e88170c6ce855fef0
Jaiveer Singh <jsboygenius@jaiveers-macbook-pro.local>
2018-09-21 22:42:28 -0700

    Initial Commit

---
+++ Attendy IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Attendy IOS
+//
+//  Created by Jaiveer Singh on 9/21/18.
+//  Copyright © 2018 Full Stack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
